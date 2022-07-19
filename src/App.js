d9c36162cda3c9da5bdacb53055485dba5d9d70d
Thomas <thomashaudiquer@gmail.com>
2022-07-19 17:30:06 +0100

    adding button and rolling dice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import Confetti from 'react-confetti'
+import Die from './Components/Die'
+import React, {useState, useEffect} from 'react'
+import {nanoid} from "nanoid"
+
 
 function App() {
-  return (
-    <div className="App">
-      <main>
-      <h1>Tenzies</h1>
-      <div className="container"></div>
-      </main>
-    </div>
-  );
-}
 
+  const [dice, setDice] = React.useState(allNewDice())
+    
+  function allNewDice() {
+      const newDice = []
+      for (let i = 0; i < 10; i++) {
+          newDice.push({
+              value: Math.ceil(Math.random() * 6), 
+              isHeld: false,
+              id: nanoid()
+          })
+      }
+      console.log(newDice)
+      return newDice
+  }
+  
+  function rollDice() {
+      setDice(allNewDice())
+  }
+  
+  const diceElements = dice.map(die => (
+      <Die key={die.id} value={die.value} />
+  ))
+  
+  return (
+      <main>
+          <div className="dice-container">
+              {diceElements}
+          </div>
+          <button className="roll-dice" onClick={rollDice}>Roll</button>
+      </main>
+  )
+}
 export default App;
---
+++ src/Components/Die.js
@@ -0,0 +1,7 @@
+export default function die(props) {
+    return (
+        <div className="die-face">
+            <h2 className="die-num">{props.value}</h2>
+        </div>
+    )
+}
